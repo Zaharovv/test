141dbac6b9a33200b9111c642c73baf0765984dd
zaharovv <zaharov.v@livetex.ru>
2015-08-17 16:40:27 +0300

    second commit

---
+++ HiLoad/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace HiLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            Console.WriteLine(start);
+            FireFox();
+            Chrome();
+
+            DateTime end = DateTime.Now;
+            Console.WriteLine(end);
+            Console.Read();
+
+        }
+
+        private static async void FireFox()
+        {
+            IWebDriver FfDriver = new FirefoxDriver();
+            int counter = 0;
+            string url = "http://status-service.testing-05.livetex/?method=label.isAvailable&siteId=10015983";
+            for (int i = 0; i < 10000; i++)
+            {
+                FfDriver.Navigate().GoToUrl(url);
+                await TestAsync(i);
+                counter++;
+            }
+            Console.WriteLine("FF - " + counter);
+            DateTime end = DateTime.Now;
+            Console.WriteLine("FFr - " + end);
+        }
+        private static async void Chrome()
+        {
+            IWebDriver FfDriver = new ChromeDriver();
+            int counter = 0;
+            string url = "http://status-service.testing-05.livetex/?method=label.isAvailable&siteId=10015983";
+            for (int i = 0; i < 10000; i++)
+
+            {
+                FfDriver.Navigate().GoToUrl(url);
+                await TestAsync(i);
+                counter++;
+            }
+            Console.WriteLine("Chrome - " + counter);
+            DateTime end = DateTime.Now;
+            Console.WriteLine("Chrome - " + end);
+        }
+
+        private static Task TestAsync(int i)
+        {
+
+            return Task.Run(() => TaskToDo(i));
+        }
+
+        private async static void TaskToDo(int i)
+        {
+            await Task.Delay(1);
+
+        }
+
+
+    }
+}
+
+
+
+
+
